c5e9aa2d34cc5ecbf24c85352cad0858b8b01fa2
Karthik171819 <karthikkumar092004@gmail.com>
2025-09-22 20:18:46 +0530

    app viewing

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyparser = require('body-parser');
 const exhbs = require('express-handlebars');
 
 //configuration, views used for templating engine purpose
-app.engine('hbs', exhbs.engine());
+app.engine('hbs', exhbs.engine({layoutsDir:'views/', defaultLayout:"main", extname:"hbs"}));
 app.set('view engine', 'hbs');
 app.set('views', 'views');
 
@@ -12,4 +12,9 @@ app.set('views', 'views');
 app.get('/', (req, res) =>{
     let message = 'text';
     res.render('main',{message});
+})
+
+//run in the port
+app.listen(8000, () =>{
+    console.log('listening to 8000 port');
 })

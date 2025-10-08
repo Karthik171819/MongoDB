b46dc85df7efef1129b0c6f088870dc7b9513de2
Karthik171819 <karthikkumar092004@gmail.com>
2025-10-08 20:16:44 +0530

    mongoose

--- db.js
+++ db.js
@@ -1,18 +1,22 @@
 //connecting mongodb
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient; //driver
-const ObjectID = mongodb.ObjectId;
+// const mongodb = require('mongodb'); -r for mongodb
+// const MongoClient = mongodb.MongoClient; //driver -r for mongodb
+// const ObjectID = mongodb.ObjectId; -r for mongodb
 
-let database;
+//mongoose
+const mongoose = require('mongoose');
+
+// let database; -r for mongodb
 
 async function getDatabase() {
-    const client = await MongoClient.connect('mongodb://127.0.0.1/27017');
-    database = client.db('library');
+    // const client = await MongoClient.connect('mongodb://127.0.0.1:27017')
+    // database = client.db('library');
 
-    if(!database) {
-        console.log("Dtabase not connected");
-    }
-    return database;
+    // if(!database) {
+    //     console.log("Dtabase not connected");
+    // }
+    // return database; -r for mongodb
+    mongoose.connect('mongodb://127.0.0.1:27017/library')
 }
 
 module.exports = {

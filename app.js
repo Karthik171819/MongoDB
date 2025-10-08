9efa4d75a0805d6ec7bc50f6589dd934a64a9f3b
Karthik171819 <karthikkumar092004@gmail.com>
2025-10-08 18:42:59 +0530

    app.js

--- app.js
+++ app.js
@@ -2,17 +2,54 @@ const express = require('express'); //express package
 const app = express();
 const bodyparser = require('body-parser');
 const exhbs = require('express-handlebars');
+const dbo = require('./db');
+const ObjectID = dbo.ObjectID;
 
 //configuration, views used for templating engine purpose
 app.engine('hbs', exhbs.engine({layoutsDir:'views/', defaultLayout:"main", extname:"hbs"}));
 app.set('view engine', 'hbs');
 app.set('views', 'views');
+app.use(bodyparser.urlencoded({extended : true }))
+
 
 //create routing
-app.get('/', (req, res) =>{
-    let message = 'text';
-    res.render('main',{message});
+app.get('/', async (req, res) =>{
+    let database = await dbo.getDatabase();
+    const collection = database.collection('books');
+    const cursor = collection.find({});
+    let books = await cursor.toArray();
+
+    let message = '';
+    let edit_id, edit_book;
+
+    if(req.query.edit_id){
+        edit_id = req.query.edit_id;
+        edit_book = await  collection.findOne({_id:  new ObjectID(edit_id)}) //binary work behind mongodb
+    }
+
+    switch(req.query.status){
+        case '1':
+            message = "Inserted successfully";
+            break;
+
+        default:
+            break;
+    }
+
+    res.render('main',{message, books, edit_id, edit_book});
 })
+
+app.post('/store_book', async(req, res) => {
+    let database = await dbo.getDatabase();
+    const collection = database.collection('books');
+    let book = {title: req.body.title, author: req.body.author} 
+    await collection.insertOne(book);
+    return res.redirect('/?status=1');
+
+
+})
+
+
 
 //run in the port
 app.listen(8000, () =>{
